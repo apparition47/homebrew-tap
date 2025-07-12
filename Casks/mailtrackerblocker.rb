8b3805a18b9fe88678bb09beae72facf4c04baf1
Aaron Lee <aaron@onefatgiraffe.com>
2025-07-12 21:41:22 +0900

    Update mailtrackerblocker.rb

--- Casks/mailtrackerblocker.rb
+++ Casks/mailtrackerblocker.rb
@@ -8,7 +8,7 @@ cask "mailtrackerblocker" do
   desc "Email tracker, read receipt and spy pixel blocker plugin for Apple Mail"
   homepage "https://apparition47.github.io/MailTrackerBlocker/"
 
-  deprecate! date: "2024-04-22", because: :moved_to_mas
+  no_autobump! because: :bumped_by_upstream
   
   auto_updates true
   depends_on macos: [
